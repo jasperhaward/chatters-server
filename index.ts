9e8350ea887d7526d3305734c5ccdb19b21649ce
Jasper Haward <jasperhaward@gmail.com>
2023-07-08 21:34:35 +0100

    fix: force remove build folder & update build folder path

---
+++ index.ts
@@ -0,0 +1,6 @@
+import App from "./src/app";
+import config from "./src/config";
+
+const app = new App(config);
+
+app.start();
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import App from "./app";
-import config from "./config";
-
-const app = new App(config);
-
-app.start();
