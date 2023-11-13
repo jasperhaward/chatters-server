e212691b4288b12a6d92dd43b8eb8bc89072f959
Jasper Haward <jasperhaward@gmail.com>
2023-11-13 14:26:07 +0000

    feat: migration for conversation_latest_message view creation

---
+++ src/migrations/2023-11-13_add-conversation-latest-message-view.ts
@@ -0,0 +1,30 @@
+import { Kysely } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createView("conversation_latest_message")
+    .as(
+      db
+        .selectFrom("conversation as c")
+        .leftJoin(
+          "conversation_message as m",
+          "m.conversation_id",
+          "c.conversation_id"
+        )
+        .distinctOn("c.conversation_id")
+        .select([
+          "c.conversation_id",
+          "m.id as latest_message_id",
+          "m.created_at as latest_message_created_at",
+          "m.created_by as latest_message_created_by",
+          "m.content as latest_message_content",
+        ])
+        .orderBy("c.conversation_id")
+        .orderBy("m.created_at", "desc")
+    )
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropView("conversation_latest_message").execute();
+}
