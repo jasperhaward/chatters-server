9e8e5a39614e5771e5506d90adc6560c0a418c84
Jasper Haward <jasperhaward@gmail.com>
2023-11-13 14:15:14 +0000

    feat: conversation_latest_message view schema

--- schema.sql
+++ schema.sql
@@ -26,6 +26,16 @@ CREATE TABLE conversation_message(
     content varchar(250) NOT NULL
 );
 
+CREATE VIEW conversation_latest_message AS
+    SELECT DISTINCT ON (c.conversation_id) c.conversation_id,
+        m.id AS latest_message_id,
+        m.created_at AS latest_message_created_at,
+        m.created_by AS latest_message_created_by,
+        m.content AS latest_message_content
+    FROM conversation c
+    LEFT JOIN conversation_message m ON m.conversation_id = c.conversation_id
+    ORDER BY c.conversation_id, m.created_at DESC;
+
 CREATE TABLE conversation_recipient(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     created_at timestamptz DEFAULT current_timestamp,
