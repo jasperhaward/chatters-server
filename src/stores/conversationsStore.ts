cde43ba64ce9af749a9d1af9e1cec561f7e344be
Jasper Haward <jasperhaward@gmail.com>
2024-10-20 17:23:41 +0100

    fix: order of conversations when a user was added

--- src/stores/conversationsStore.ts
+++ src/stores/conversationsStore.ts
@@ -96,14 +96,24 @@ export async function findConversationsByUserId(
   db: Kysely<Database>,
   userId: string
 ): Promise<TConversationWithoutRecipients[]> {
+  const { max } = db.fn;
+
   const rows = await db
-    .selectFrom("conversation_recipient_es as r")
+    .selectFrom("conversation_recipient_es as ur")
     .innerJoin(
       "conversation_creation_es as c",
       "c.conversation_id",
-      "r.conversation_id"
+      "ur.conversation_id"
     )
-    .innerJoin("user_account as cu", "cu.user_id", "c.created_by")
+    .innerJoin(
+      db
+        .selectFrom("conversation_recipient_es")
+        .select([max("created_at").as("max_created_at"), "conversation_id"])
+        .groupBy("conversation_id")
+        .as("r"),
+      "r.conversation_id",
+      "c.conversation_id"
+    )
     .leftJoin(
       "conversation_title_es as t",
       "t.conversation_id",
@@ -114,22 +124,21 @@ export async function findConversationsByUserId(
       "m.conversation_id",
       "c.conversation_id"
     )
-    .leftJoin("user_account as mu", "mu.user_id", "m.created_by")
     .select([
       "c.conversation_id",
       "c.created_at",
       "c.created_by",
-      "cu.username as created_by_username",
+      "c.created_by_username",
       "t.title",
       "m.id as latest_message_id",
       "m.created_at as latest_message_created_at",
       "m.created_by as latest_message_created_by",
-      "mu.username as latest_message_created_by_username",
+      "m.created_by_username as latest_message_created_by_username",
       "m.message as latest_message_content",
     ])
-    .where("r.recipient_id", "=", userId)
+    .where("ur.recipient_id", "=", userId)
     .orderBy(
-      sql`greatest(c.created_at, t.created_at, r.created_at, m.created_at)`,
+      sql`greatest(c.created_at, t.created_at, r.max_created_at, m.created_at)`,
       "desc"
     )
     .execute();
